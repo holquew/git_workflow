0a939b5dd542ed9b81891bc447ef8cb84950b383
holquew <holquew@gmail.com>
2017-12-04 22:34:35 +0900

    Fill main.c with code that prints jugg

--- git_workflow/main.c
+++ git_workflow/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "Juggling_Algo.h"
+#include "ming.h"
 
 int main() {
-
+	int arr[6] = { 1,2,3,4,5,6 };
+	int *jugg = Juggling(arr, 6, 2);
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", jugg[i]);
+	}
+	printf("\n");
+	free(jugg);
+	return 0;
 }
